b62c607c6e06643bc7ff1d2fd01e6dd09ac40f26
yasincelebi <yasincelebix@gmail.com>
2021-12-13 01:18:25 +0300

    format user service

--- api/src/services/users.ts
+++ api/src/services/users.ts
@@ -1,41 +1,59 @@
-import { User, PrismaClient } from '@prisma/client';
+import { User, PrismaClient, Prisma } from '@prisma/client';
+
 import logger from '../scripts/logger/users';
 
 const prisma = new PrismaClient();
-export const insert = async (_user: User): Promise<User | null | undefined> => {
-  const create = await prisma.user
-    .create({
-      data: { ..._user, createdAt: new Date(), updatedAt: new Date() },
-    })
-    .catch((err) => err);
-  return create;
-};
 
-export const findUserById = async (data: { id: string }): Promise<User | null> => {
-  const { id } = data;
-  console.log(id);
-  const user = await prisma.user.findUnique({
-    where: {
-      id,
-    },
-  });
+export default class UserService {
+  public insert = async (user: User): Promise<User | null | undefined> => {
+    const create = await prisma.user
+      .create({
+        data: user,
+      })
+      .catch((err) => err);
+    console.log(create);
+    return create;
+  };
 
-  return user;
-};
+  public findUserById = async (data: { id: string }): Promise<User | null> => {
+    const { id } = data;
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+    console.log(user);
 
-export const findUserByEmail = async (data: { email: string }): Promise<User | null> => {
-  const { email } = data;
-  const user = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  });
-  return user;
-};
+    return user;
+  };
+
+  public static findUserByEmail = async (data: { email: string }): Promise<User | null> => {
+    const { email } = data;
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    return user;
+  };
+
+  public static update = async (data: {
+    where: Prisma.UserWhereUniqueInput;
+    willUpdateData: any;
+  }): Promise<User | null> => {
+    const { where, willUpdateData } = data;
+    const user = await prisma.user.update({
+      where,
+      data: { ...willUpdateData, updatedAt: new Date() },
+    });
+
+    return user;
+  };
+}
 
 prisma.$use(async (params: any, next) => {
   logger.log({ level: 'info', message: params, service: 'user-service' });
   const result = await next(params);
-  console.log(result);
   return result;
 });
