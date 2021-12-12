ef48f058f4800b5b60d2bfb6564cab3875714946
yasincelebi <yasincelebix@gmail.com>
2021-12-13 01:17:05 +0300

    try reset user password

--- api/src/api-routes/user.ts
+++ api/src/api-routes/user.ts
@@ -1,14 +1,22 @@
 import Express from 'express';
 import Validator from '../middlewares/validate';
 import UsersController from '../controllers/users';
-import { createUserValidation, loginUserValidation } from '../validations/users';
+import {
+  createUserValidation,
+  loginUserValidation,
+  resetPasswordValidation,
+} from '../validations/users';
 import Authenticate from '../middlewares/authenticate';
 
 const auth = new Authenticate();
 const router = Express.Router();
 const usersController = new UsersController();
+
 router.route('/').get(auth.handle, usersController.getUser);
 router.route('/signup').post(Validator.validate(createUserValidation), usersController.createUser);
 router.route('/login').post(Validator.validate(loginUserValidation), usersController.loginUser);
+router
+  .route('/reset-password')
+  .post(Validator.validate(resetPasswordValidation), usersController.resetPassword);
 
 export default router;
