4d9ee6288518d6c26c17cec5d6fa9bf2597227e6
yasincelebi <yasincelebix@gmail.com>
2021-12-13 01:18:37 +0300

    add user password validation

--- api/src/validations/users.ts
+++ api/src/validations/users.ts
@@ -2,14 +2,10 @@ import Joi from 'joi';
 
 // user validation
 const createUserValidation = Joi.object({
-  id: Joi.string().required(),
-  name: Joi.string().required().min(3).max(30),
   email: Joi.string().email().required(),
   password: Joi.string().required(),
   createdAt: Joi.date(),
   updatedAt: Joi.date(),
-  companyId: Joi.any(),
-  contactId: Joi.any(),
 });
 
 const loginUserValidation = Joi.object({
@@ -17,7 +13,11 @@ const loginUserValidation = Joi.object({
   password: Joi.string().required(),
 });
 
-export { loginUserValidation, createUserValidation };
+const resetPasswordValidation = Joi.object({
+  email: Joi.string().email().required(),
+});
+
+export { loginUserValidation, createUserValidation, resetPasswordValidation };
 
 export declare interface ICreateUser {
   name: string;
