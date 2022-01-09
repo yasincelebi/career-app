eb6efb9da8cb5c69e65993ab0f7ed33e83548bd8
yasincelebi <yasincelebix@gmail.com>
2022-01-09 20:13:17 +0300

    add login user type

--- api/src/graphql/schemas/user.ts
+++ api/src/graphql/schemas/user.ts
@@ -10,15 +10,26 @@ const userTypeDefs = gql`
   type Query {
     allUsers: [User!]!
     oneUser(id: ID!): User
+    users: [User!]!
   }
   input UserInput {
     email: String!
     name: String
     id: ID!
+    password: String!
+  }
+  type LoginUser {
+    email: String!
+    id: ID!
+    accessToken: String!
+    refreshToken: String!
+    name: String!
+    password: String
   }
   type Mutation {
     createUser(value: UserInput!): User
     deleteUser(id: ID!): User
+    loginUser(email: String!, password: String!): LoginUser
   }
 `;
 
