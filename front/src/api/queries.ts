7a34537ff996fe22fd2ca35d1d20cebe2224920d
yasincelebi <yasincelebix@gmail.com>
2022-01-09 20:25:17 +0300

    add graphql queries

---
+++ front/src/api/queries.ts
@@ -0,0 +1,35 @@
+import { gql } from '@apollo/client'
+
+export const GET_ALL_USERS = gql`
+  query {
+    allUsers {
+      id
+      name
+      email
+      password
+    }
+  }
+`
+
+export const REGISTER_USER = gql`
+  mutation CreateUser($value: UserInput!) {
+    createUser(value: $value) {
+      id
+      name
+      email
+    }
+  }
+`
+
+export const LOGIN_USER = gql`
+  mutation LoginUser($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+      email
+      id
+      accessToken
+      refreshToken
+      name
+      password
+    }
+  }
+`
