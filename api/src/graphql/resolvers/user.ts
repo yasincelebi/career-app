84db9ac8debc7bfe3d5092091ebebf77b3c0aae9
yasincelebi <yasincelebix@gmail.com>
2022-01-09 20:13:05 +0300

    add login jwt cookie

--- api/src/graphql/resolvers/user.ts
+++ api/src/graphql/resolvers/user.ts
@@ -2,6 +2,8 @@ import { User } from '@prisma/client';
 import UserUtils from '../../scripts/utils/userUtils';
 import UserService from '../../services/userService';
 
+const { decodedToken } = require('../../services/decodedtoken');
+
 const userUtils = new UserUtils();
 const userService = new UserService();
 const userResolvers = {
@@ -9,6 +11,17 @@ const userResolvers = {
     allUsers: () => userService.listAll(),
     oneUser: (_: any, { where, value }: { where: string; value: string }) =>
       userService.find({ where, value }),
+    users: (
+      _root: any,
+      _args: any,
+      { prisma, req }: { prisma: any; req: any },
+      _info: any,
+    ): any => {
+      const decoded = decodedToken(req);
+      if (decoded) {
+        return prisma.users();
+      }
+    },
   },
   Mutation: {
     createUser: (_: any, { value }: { value: User }) => {
@@ -17,6 +30,19 @@ const userResolvers = {
     },
     deleteUser: (_: any, { where, value }: { where: string; value: string }) =>
       userService.delete({ where, value }),
+    loginUser: (_: any, { email, password }: { email: string; password: string }, context: any) =>
+      userService.loginUser({ email, password }).then((user) => {
+        context.req.res.cookie('accessToken', user.accessToken, {
+          httpOnly: true,
+          maxAge: 1000 * 60 * 60 * 24 * 7,
+        });
+        context.req.res.cookie('refreshToken', user.refreshToken, {
+          httpOnly: true,
+          maxAge: 1000 * 60 * 60 * 24 * 7,
+        });
+
+        return user;
+      }),
   },
 };
 
