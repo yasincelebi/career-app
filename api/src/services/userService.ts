abc7aa4e7cbf2867458f1e19feea5aabb7c1b1f8
yasincelebi <yasincelebix@gmail.com>
2022-01-09 20:14:25 +0300

    implement login service

--- api/src/services/userService.ts
+++ api/src/services/userService.ts
@@ -1,11 +1,12 @@
 /* eslint-disable class-methods-use-this */
 import { PrismaClient, User } from '@prisma/client';
 
-import logger from '../scripts/logger/users';
+import UserUtils from '../scripts/utils/userUtils';
+
 import BaseService from './baseService';
 
 const prisma = new PrismaClient();
-
+const userUtils = new UserUtils();
 export default class UserService extends BaseService {
   constructor() {
     super('user');
@@ -17,10 +18,31 @@ export default class UserService extends BaseService {
     });
     return result;
   };
+
+  public loginUser = async ({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }): Promise<LoginUser> => {
+    const user = await (prisma as any).user.findUnique({
+      where: { email },
+    });
+
+    if (user && user?.password === userUtils.hashPassword(password)) {
+      return {
+        ...user,
+        accessToken: userUtils.generateAccessToken(user),
+        refreshToken: userUtils.generateRefreshToken(user),
+      };
+    }
+    return user;
+  };
 }
 
-prisma.$use(async (params: any, next) => {
-  logger.log({ level: 'info', message: params, service: 'user-service' });
-  const result = await next(params);
-  return result;
-});
+export interface LoginUser extends User {
+  accessToken: string;
+  refreshToken: string;
+  error: string;
+}
