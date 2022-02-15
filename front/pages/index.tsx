23cf3f22c968d54a2e1b7608746c510946ace2b1
yasincelebi <yasincelebix@gmail.com>
2022-02-15 22:59:24 +0300

    ..

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,7 +1,9 @@
-import type {NextPage} from "next";
+import type { NextPage } from 'next'
+import Spinner from '../components/shared/Spinner/Spinner'
+import { useLoading } from '../src/hooks/useLoading'
 
 const Home: NextPage = () => {
-    return <div className="w-10">asddsa</div>;
-};
+  return <div className="w-10">asddsa</div>
+}
 
-export default Home;
+export default Home
