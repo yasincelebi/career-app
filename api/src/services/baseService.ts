649534b447292b0a12ab944fad10b067cc9dee04
yasincelebi <yasincelebix@gmail.com>
2022-02-15 22:57:40 +0300

    update generic model

--- api/src/services/baseService.ts
+++ api/src/services/baseService.ts
@@ -1,61 +1,41 @@
-import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+export default abstract class BaseService<T> {
 
-type IBaseModel = keyof typeof prisma;
-
-export default class BaseService {
-  BaseModel: IBaseModel;
-
-  constructor(model: IBaseModel) {
-    this.BaseModel = model;
+  constructor() {
+    if (new.target === BaseService) {
+      throw new Error('BaseService is an abstract class and cannot be instantiated directly.');
+    }
   }
 
-  public find = async ({ where, value }: { where: string; value: any }): Promise<any> => {
-    const result = await (prisma as any)[this.BaseModel].findUnique({
-      where: {
-        [where]: value,
-      },
-    });
-    return result;
+  // @ts-ignore
+  // eslint-disable-next-line no-unused-vars
+  public find = async ({where, value}: { where: string; value: any }): Promise<T | null> => {
+    throw new Error('Method not implemented.');
   };
 
-  public listAll = async (): Promise<void> => {
-    const result = await (prisma as any)[this.BaseModel].findMany();
-    return result;
+  public listAll = async (): Promise<T[]> => {
+    throw new Error('Method not implemented.');
   };
 
-  public create = async ({ value }: { value: any }): Promise<any> => {
-    const result = await (prisma as any)[this.BaseModel].create({
-      data: value,
-    });
-    return result;
+  // @ts-ignore
+  // eslint-disable-next-line no-unused-vars
+  public create = async ({value}: { value: any }): Promise<T> => {
+    throw new Error('Method not implemented.');
   };
 
-  public update = async <T>({
-    where,
-    value,
-    data,
-  }: {
+  // @ts-ignore
+  // eslint-disable-next-line no-unused-vars
+  public update = async ({where, value, data}: {
     where: string;
-    value: any;
-    data: T;
-  }): Promise<T> => {
-    const result = await (prisma as any)[this.BaseModel].update({
-      where: {
-        [where]: value,
-      },
-      data,
-    });
-    return result;
+    value: string;
+    data: any;
+  }): Promise<any> => {
+    throw new Error('Method not implemented.');
   };
 
-  public delete = async ({ where, value }: { where: string; value: string }): Promise<void> => {
-    const result = await (prisma as any)[this.BaseModel].delete({
-      where: {
-        [where]: value,
-      },
-    });
-    return result;
+  // @ts-ignore
+  // eslint-disable-next-line no-unused-vars
+  public delete = async ({where, value}: { where: string; value: string }): Promise<T | null> => {
+    throw new Error('Method not implemented.');
   };
 }
